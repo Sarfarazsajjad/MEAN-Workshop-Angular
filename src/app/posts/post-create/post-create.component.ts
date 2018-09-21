a08a4109ff801a01ae9a4b5568164d82904bb1ad
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-09-21 15:38:06 +0500

    creating a click event handler in component class and applying it on a button using Angular event binding syntax (event)="handler()"

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,7 @@ import { Component } from "@angular/core";
   templateUrl:"./post-create.component.html"
 })
 export class PostCreateComponent{
-
+  onSavePost(){
+    console.log('save post');
+  }
 }
