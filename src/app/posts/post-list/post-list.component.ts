212e6ece76cc75a988939393c7ae39659ac4d1dc
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-09-25 18:29:33 +0500

    Using *ngFor to show list items and *ngIf for conditional elements

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,6 +6,13 @@ import { Component } from "@angular/core";
   styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent{
+export class PostListComponent {
 
+  // posts = [
+  //   { postTitle: 'Post One', postContent: 'This is Post One Content' },
+  //   { postTitle: 'Post Two', postContent: 'This is Post Two Content' },
+  //   { postTitle: 'Post Three', postContent: 'This is Post Three Content' },
+  // ];
+
+  posts = [];
 }
