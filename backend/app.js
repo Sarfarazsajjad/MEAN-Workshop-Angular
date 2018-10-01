c47de55d108ace8e2f951b44ef2db1ba346f7007
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-10-01 22:29:02 +0500

    creating an express route and sending json data with 200 success status code and json formating

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-  console.log('first middleware');
-  next();
-})
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "saldskfj",
+      title: "post 1",
+      content: "post 1 content from server"
+    },
+    {
+      id: "kljhfafa",
+      title: "post 2",
+      content: "post 2 content from server"
+    }
+  ];
 
-app.use((req,res,next) => {
-  console.log('second middleware');
-  res.send('hello from express');
-})
+  res.status(200).json({
+    message: 'Posts fetched from server successfully',
+    posts: posts
+  });
+});
 
 module.exports = app;
