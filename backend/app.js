bbf1fd73742f90af09e01fa72f9ce9bc0aa685b2
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-10-03 00:29:33 +0500

    get data from database with find method on the model object. and send to client side from get method on server side

--- backend/app.js
+++ backend/app.js
@@ -34,23 +34,14 @@ app.post('/api/posts', (req, res, next)=>{
 })
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: "saldskfj",
-      title: "post 1",
-      content: "post 1 content from server"
-    },
-    {
-      id: "kljhfafa",
-      title: "post 2",
-      content: "post 2 content from server"
-    }
-  ];
+  Post.find().then((postDocuments)=>{
+    res.status(200).json({
+      message: 'Posts fetched from server successfully',
+      posts: postDocuments
+    });
+  })
 
-  res.status(200).json({
-    message: 'Posts fetched from server successfully',
-    posts: posts
-  });
+  
 });
 
 module.exports = app;
