1668c14a05ed4f6ecfecee1957fc66602f5879c2
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-11-04 17:44:30 +0500

    bcrypt hash user password - Creating a new user upon request

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const brcypt = require('bcrypt');
 
 router.post("/signup", (req, res, next) => {
+  brcypt.hash(req.body.password, 10)
+    .then(hash => {
+      let user = new User({
+        email: req.body.email,
+        password: hash
+      })
 
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: 'User Created',
+            result: result
+          })
+        }).catch(err => {
+          res.status(500).json({
+            error: err
+          })
+        });
+
+    });
 });
 
 module.exports = router;
