ac87299ddf2f5bd56d9595ef620da7f5b9f0c22a
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-11-04 15:05:07 +0500

    Mongoose validator, User Router, User Model -  Creating User Model

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 app.use(bodyParser.json());
 
 app.use('/api/posts',postsRoutes);
+app.use('/api/user',userRoutes);
 
 module.exports = app;
