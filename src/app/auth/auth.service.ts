fc8c28458b18ed0c03788bd39a596600cf55dbde
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-11-06 14:23:26 +0500

    redirect to default page on login logout - redirecting the users

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AuthData } from "./auth-data.model";
 import { Subject } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
   private token: string;
   private authListner = new Subject<boolean>();
   private authenticated:boolean = false;
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   getAuthenticationListener() {
     return this.authListner.asObservable();
@@ -38,6 +40,7 @@ export class AuthService {
           this.token = response.token;
           this.authenticated = true;
           this.authListner.next(true);
+          this.router.navigate(['/']);
         }
       });
   }
@@ -46,5 +49,6 @@ export class AuthService {
     this.token = null;
     this.authenticated = false;
     this.authListner.next(false);
+    this.router.navigate(['/']);
   }
 }
